3bceb5a859f633985349ccb430682ae471c770e9
Oskar Jaskólski <oskarrro90@gmail.com>
2022-12-23 12:47:40 +0100

    )) |'ve added a fullscreen option via double-click anywhere on the page. )) Now the feather info icon is fading out after 3s, and it appears every time the user hovers it with the mouse. )) I've changed a little bit formatting of the code

--- modal.js
+++ modal.js
@@ -1,15 +1,20 @@
-var modal = document.querySelector('[data-modal]');
+const modal = document.querySelector("[data-modal]");
 
-modal.addEventListener('click', function (event) {
+modal.addEventListener("click", function (event) {
   event.preventDefault();
-  var modalId = document.getElementById(modal.dataset.modal);
-  modalId.classList.add('open');
+  const modalId = document.getElementById(modal.dataset.modal);
+  modalId.classList.add("open");
 
-  var exits = modalId.querySelectorAll('.modal-exit');
+  var exits = modalId.querySelectorAll(".modal-exit");
   exits.forEach(function (exit) {
-    exit.addEventListener('click', function (event) {
+    exit.addEventListener("click", function (event) {
       event.preventDefault();
-      modalId.classList.remove('open');
+      modalId.classList.remove("open");
     });
   });
 });
+
+setTimeout(() => {
+  const featherInfo = document.getElementsByClassName("feather-info")[0];
+  featherInfo.classList.toggle("feather-info-hover");
+}, 3000);
